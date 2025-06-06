0b8e3e22bf0ae3147d7ff6cf750e809e9425c2dc
ItsGunesh <itsme.gunesh87@gmail.com>
2025-06-06 19:18:06 +0530

    Appwrite Authenticaion Done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_TEST)
+
+  return (
+
+    <>
+      <h1>Blog app with vite !</h1>
+    </>
+  )
+}
+
+export default App
